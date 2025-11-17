fa1e36f569e035772c937229d2fd1694b4d6e87a
shravaanr <shravanramakunja1@gmail.com>
2025-11-17 23:54:40 +0530

    feat:created files

---
+++ .history/index_20251116223531.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// To read JSON from POST request body
+app.use(express.json());
+
+// GET Route
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// POST Route
+app.post('/create', (req, res) => {
+    const data = req.body; // whatever the client sends
+    res.send({
+        message: "POST request received!",
+        yourData: data
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
---
+++ .history/tailwind.config_20251117235035.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./views/**/*.{ejs,html,js}",
+    "./public/js/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
